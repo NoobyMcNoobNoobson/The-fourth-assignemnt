764debf49a3a41c8cef7d6a4b935579fb9cde0ef
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-03-13 09:07:03 -0600

    laptop

--- The code for number one.py
+++ The code for number one.py
@@ -8,6 +8,9 @@ def main():
     surverys_df = pd.read_csv('surveys.csv')
     sns.set()
     sns.relplot(x = 'weight',y='hindfoot_length',data = surverys_df,kind= 'scatter')
+    #Every data point represents two things:- 1. Weight and 2. Hindfoot length 
+    # it seems the data had two dense clusters of data points that are there thansk to some correlation, 
+    #let's see if we can find what correlatation to be exact 
 
 main()
 
