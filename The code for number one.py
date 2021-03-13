33daed690e97d53aa5e362c98bd27fb073f54dff
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-03-12 23:15:29 -0600

    laptop

--- The code for number one.py
+++ The code for number one.py
@@ -1,8 +1,12 @@
 import seaborn as sns 
 import pandas as pd 
 import matplotlib as plt
+import os
+from os import path
 
 def main():
     surverys_df = pd.read_csv('surveys.csv')
     weight_and_hindfoot_df = surverys_df.groupby(['weight','hindfoot_length'])
-    
+
+main()
+
