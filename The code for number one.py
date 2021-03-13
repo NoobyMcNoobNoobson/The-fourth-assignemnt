d66ed306b212e3ae2dc27d2639e098f66674eec1
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-03-12 23:16:16 -0600

    laptop

--- The code for number one.py
+++ The code for number one.py
@@ -7,6 +7,7 @@ from os import path
 def main():
     surverys_df = pd.read_csv('surveys.csv')
     weight_and_hindfoot_df = surverys_df.groupby(['weight','hindfoot_length'])
+    print(weight_and_hindfoot_df.shape)
 
 main()
 
